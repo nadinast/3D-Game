713b6f3542e545a263be9d8af93658ab187e6337
Nadina Streanga <nadistreanga@gmail.com>
2018-04-29 15:39:26 +0300

    Added part of game logic and game components

--- js/game.js
+++ js/game.js
@@ -1,3 +1,16 @@
+function onUserKeyDown(event) {
+    var keyCode = event.which;
+    if(keyCode == 37)  //left key pressed
+        catcher.position.x -= 4;
+    else if(keyCode == 39) // right key pressed
+        catcher.position.x += 4;
+    else if(keyCode == 38)
+        catcher.position.z -= 4;
+    else if(keyCode == 40)
+        catcher.position.z += 4;
+}
+document.addEventListener("keydown", onUserKeyDown, false);
+
 var colorArray = ["yellow", "red", "green", "blue"];
 
 var aspect = window.innerWidth / window.innerHeight;
@@ -5,55 +18,98 @@ var aspect = window.innerWidth / window.innerHeight;
 var canvas = document.getElementById("glcanvas");
 
 var renderer = new THREE.WebGLRenderer({canvas: canvas});
+
 var scene = new THREE.Scene();
 scene.background =  new THREE.Color("skyblue");
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
-camera.position.z = 5;
+var camera = new THREE.PerspectiveCamera( 60, aspect, 0.1, 1000 );
+camera.position.z = 20;
 
-var spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(-50, 50, 20);
-scene.add(spotLight);
+var loader = new THREE.TextureLoader();
 
-var material1 = new THREE.MeshPhongMaterial();
-var material2  = new THREE.MeshPhongMaterial();
-var material3  = new THREE.MeshPhongMaterial();
-var material4  = new THREE.MeshPhongMaterial();
-var geometry = new THREE.BoxGeometry( 0.25, 0.25, 0.25 );
-var cube1 = new THREE.Mesh( geometry, material1 );
-var cube2 = new THREE.Mesh( geometry, material2 );
-var cube3 = new THREE.Mesh( geometry, material3 );
-var cube4 = new THREE.Mesh( geometry, material4 );
+var geometry = new THREE.SphereGeometry(0.5, 32, 32);
+var material1 = new THREE.MeshPhongMaterial({reflectivity: .5, refractionRatio: 1});
+var material2 = new THREE.MeshPhongMaterial({reflectivity: .5, refractionRatio: 1});
+var material3 = new THREE.MeshPhongMaterial({reflectivity: .5, refractionRatio: 1});
+var material4 = new THREE.MeshPhongMaterial({reflectivity: .5, refractionRatio: 1});;
+var material5 = new THREE.MeshPhongMaterial({reflectivity: .5, refractionRatio: 1});;
+var sphere1 = new THREE.Mesh( geometry, material1 );
+var sphere2 = new THREE.Mesh( geometry, material2 );
+var sphere3 = new THREE.Mesh( geometry, material3 );
+var sphere4 = new THREE.Mesh( geometry, material4 );
+var sphere5 = new THREE.Mesh( geometry, material5 );
+createSphere(sphere1, material1);
+createSphere(sphere2, material2);
+createSphere(sphere3, material3);
+createSphere(sphere4, material4);
+createSphere(sphere5, material5);
 
-createCube(cube1, material1);
-createCube(cube2, material2);
-createCube(cube3, material3);
-createCube(cube4, material4);
+var woodenBasket;
+createWoodenBasket(woodenBasket);
 
-function createCube(cube, material){
-    scene.add(cube);
-    modifyCube(cube, material);
+var catcherGeometry = new THREE.CylinderGeometry( 20, 20, 10, 24 );
+var catcher = new THREE.Mesh(catcherGeometry);
+createCatcher(catcher);
+
+function createCatcher(catcher){
+    var texture = loader.load("resources/pillow-texture.jpg");
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 3, 3 );
+    texture.anisotropy = 16;
+    var catcherMaterial = new THREE.MeshLambertMaterial( { map: texture } );
+    catcher.material = catcherMaterial;
+
+    catcher.receiveShadow = true;
+    catcher.castShadow = true;
+    catcher.position.set(0, -47, -85);      //change catcher position in scene
+    scene.add(catcher);
+
 }
 
-function modifyCube(cube, material){
-    var cubeXpos = Math.floor(Math.random() * 16) - 8;
-    var cubeColor = Math.floor(Math.random() * 4);
-    material.color = new THREE.Color( colorArray[cubeColor]);
-    cube.material = material;
-    cube.position.y = 5;
-    cube.position.x = cubeXpos;
+function createWoodenBasket(woodenBasket){
+    var geometry = new THREE.CylinderGeometry( 23, 10, 70, 24);
+    var texture = loader.load("resources/basket-texture.jpg");
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 5, 5 );
+    texture.anisotropy = 32;
+    var boxMaterial = new THREE.MeshLambertMaterial( { map: texture } );
+    woodenBasket = new THREE.Mesh(geometry, boxMaterial );
+
+    woodenBasket.receiveShadow = true;
+    woodenBasket.castShadow = true;
+    woodenBasket.rotation.x = 0.05;
+    woodenBasket.position.set(160, -47, -160);    //change basket position scene
+    scene.add(woodenBasket);
 }
 
-function floatCube(cube, material, rotSpeedX, rotSpeedY, fallSpeedY){
-    cube.rotation.x += rotSpeedX;
-    cube.rotation.y += rotSpeedY;
-    if(cube.position.y > -5)
-        cube.position.y -= fallSpeedY;
+function createSphere(sphere, material){
+    sphere.castShadow = true;
+    scene.add(sphere);
+    modifySphere(sphere, material);
+}
+
+function modifySphere(sphere, material){
+    var sphereXpos = Math.floor(Math.random() * 20) - 10;
+    var sphereColor = Math.floor(Math.random() * 4);
+    material.color = new THREE.Color( colorArray[sphereColor]);
+    sphere.material = material;
+    sphere.position.z = -Math.floor(Math.random() * 20);
+    sphere.position.y = Math.abs(sphere.position.z) + 20;
+    sphere.position.x = sphereXpos;
+}
+
+function floatSphere(sphere, material, rotSpeedX, rotSpeedY, fallSpeedY){
+    sphere.rotation.x += rotSpeedX;
+    sphere.rotation.y += rotSpeedY;
+    if(sphere.position.y > -12)
+        sphere.position.y -= fallSpeedY;
     else {
-        modifyCube(cube, material);
+        modifySphere(sphere, material);
     }
 }
 
@@ -61,6 +117,42 @@ var curDate = new Date();
 var then = curDate.getSeconds();
 var sumSec = 0;
 var id;
+
+function addLights(){
+    scene.add( new THREE.AmbientLight( 0xcccccc ) );
+    var spotlight = new THREE.SpotLight(0xffffff);
+    spotlight.position.set(0, 12, 12);
+    spotlight.castShadow = true;
+    spotlight.shadow.camera.far = 1000;
+    scene.add(spotlight);
+
+    var helper = new THREE.CameraHelper( spotlight.shadow.camera );
+    scene.add( helper );
+}
+
+var floor;
+
+function createFloor(){
+    var groundTexture = loader.load('resources/wooden-floor.jpg');
+    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+    groundTexture.repeat.set( 5, 5 );
+    groundTexture.anisotropy = 16;
+
+    var groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
+    floor = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), groundMaterial );
+    floor.position.y = - 50;
+    floor.rotation.x = - Math.PI / 2;
+    floor.receiveShadow = true;
+    scene.add( floor );
+}
+
+function clearCanvas(){
+    geometry.dispose();
+    material1.dispose(); material2.dispose(); material3.dispose(); material4.dispose();
+    scene.remove( sphere1 ); scene.remove( sphere2 ); scene.remove( sphere3 ); scene.remove( sphere4 );
+    renderer.setClearColor (0xffffff, 1);
+    renderer.clear(true, true, true);
+}
 
 function render() {
     var newDate = new Date();
@@ -72,21 +164,18 @@ function render() {
     }*/
     //else {
         id = requestAnimationFrame(render);
-        floatCube(cube1, material1, 0.03, 0.01, 0.1);
-        floatCube(cube2, material2, 0.06, 0.01, 0.05);
-        floatCube(cube3, material3, 0.03, 0.01, 0.13);
-        floatCube(cube4, material4, 0.03, 0.05, 0.07);
+        floatSphere(sphere1, material1, 0.03, 0.01, 0.1);
+        floatSphere(sphere2, material2, 0.04, 0.01, 0.05);
+        floatSphere(sphere3, material3, 0.03, 0.01, 0.13);
+        floatSphere(sphere4, material4, 0.05, 0.05, 0.07);
+        floatSphere(sphere5, material5, 0.09, 0.03, 0.05);
         renderer.render( scene, camera );
     //}
 };
 
-function clearCanvas(){
-    geometry.dispose();
-    material1.dispose(); material2.dispose(); material3.dispose(); material4.dispose();
-    scene.remove( cube1 ); scene.remove( cube2 ); scene.remove( cube3 ); scene.remove( cube4 );
-    renderer.setClearColor (0xffffff, 1);
-    renderer.clear(true, true, true);
-}
+createFloor();
+addLights();
 render();
+
 
 
